4ace9b195189c3707aa7377f4c5499e82f65d2b5
Eduardo Henrique Marques Ferreira <eherique@minsait.com>
2025-10-07 14:03:43 -0300

    Iniciando o projeto

---
+++ src/main/java/es/minsait/NutrIA/NutrIaApplication.java
@@ -0,0 +1,13 @@
+package es.minsait.NutrIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/minsait/NutrIA/NutrIaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.minsait.NutrIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
